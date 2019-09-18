cb64e23c791519976f9efed520f3a13e6712e8d1
Thumper Yang <thumper.yang@gmail.com>
2019-09-18 21:55:53 +0800

    完成了基于openCV的向量化（阉割版，暂未添加碰撞融合、暂停、追踪最大星体等功能。。。） 200个星体无压力，每次while循环5ms左右 1000个星体会报错（float 变 none） 添加了一个计时器，计算每次while循环耗时并输出，用于监测 删除了Universe类，因为还不太擅长用类的功能。。。

--- vectorized version/Multibody-Gravitation-Model-Vec.py
+++ vectorized version/Multibody-Gravitation-Model-Vec.py
@@ -8,13 +8,31 @@ Created on Sun Sept 1 21:45:16 2019
 Environment: py37 with pygame
 """
 
-import random, math, pygame, sys
-from pygame.locals import *
+import random, math, time
 import numpy as np
+import cv2 as cv
 
-NUM, MASS, X, Y, VX, VY = [], [], [], [], [], []
+random.seed(100)
+
+NUM, MASS, R, X, Y, VX, VY, COLOR = [], [], [], [], [], [], [], []
+AX, AY = np.array([]), np.array([])
+
 body_list = []
 body_count = 0
+
+width, height = 960, 540
+
+middle = np.array([width//2,
+                   height//2])                     # 中心点坐标
+
+body_list = []      # 星体列表
+max_count = 20     # 初始星体数量
+max_body = None     # 最大星体
+max_path = 150      # 轨迹长度
+G = 0.02            # 引力常数
+den = 1             # 密度
+
+screen = np.zeros((height, width, 3), dtype = np.uint8)
 
 class body(object):
     
@@ -33,87 +51,95 @@ class body(object):
         self.num = num                          # 编号
         NUM.append(num)
         MASS.append(mass)                       # 质量
+        r = np.cbrt(mass/den/math.pi*0.75)      # 计算半径
+        R.append(r)
         X.append(x) 				            # 横坐标
-        Y.append(y) 				            # 横坐标
+        Y.append(y) 				            # 纵坐标
         VX.append(vx) 				            # 速度X分量
         VY.append(vy) 				            # 速度Y分量
         self.col = (random.randint(0, 255),
                     random.randint(0, 255),
                     random.randint(0, 255))     # 颜色
+        COLOR.append(self.col)
         body_list.append(self)
 
+def move_pos():
+    global X, Y, VX, VY
+    X = X + VX
+    Y = Y + VY
 
+def move_vel():
+    global VX, VY, AX, AY
+    VX = VX + AX
+    VY = VY + AY
 
-class universe(object):
+def accel():
+    global AX, AY
+
+    ones = np.ones((body_count,body_count))
+    eyes = np.eye(body_count)
+
+    DIS = np.sqrt( (X*ones-(X*ones).T)**2 + (Y*ones-(Y*ones).T)**2 )
+
+    AX2d = G*(MASS*ones)*(MASS*ones).T*(X*ones - (X*ones).T) / (DIS**3 + eyes)
+    AX = np.sum(AX2d, axis = 1)
+    AY2d = G*(MASS*ones)*(MASS*ones).T*(Y*ones - (Y*ones).T) / (DIS**3 + eyes)
+    AY = np.sum(AY2d, axis = 1)
+
+def drawStars(X, Y, R, COLOR):
+
+    global screen
+    for x, y, r, color in zip(X, Y, R, COLOR):
+        screen = cv.circle(screen, (int(x), int(y)), int(r), color, -1)    # 绘制实心圆
     
-    """
-    Class:      宇宙，即向量化的所有星体
-    CreateObj:  universe(NUM, MASS, X, Y, VX, VY) 	所有参数为body的向量化版本
-    
-    模型假设:
-        所有星体密度相同
-        星体间的相互作用只有万有引力
-        星体碰撞为完全非弹性碰撞
-        碰撞时大星体吞噬小星体，没有质量损失
-        碰撞后形成的新星体位置位于两星体的质心
-    """
-    
-    def __init__(self, NUM, MASS, X, Y, VX, VY):
-        
-        """
-        Method:     __init__                    构造方法
-        Input:      NUM, MASS, X, Y, VX, VY     宇宙参数
-        Return:     universe                    宇宙类
-        """
-        self.NUM = NUM                          # 编号
-        self.MASS = MASS                        # 质量
-        self.R = self.calr()                    # 半径
-        self.X = X 				                # 横坐标
-        self.Y = Y 				                # 纵坐标
-        self.VX = VX 				            # 速度X分量
-        self.VY = VY 				            # 速度Y分量
-        self.PATH = []                          # 轨迹列表
-
-    def move_pos():
-        X = X + VX
-        Y = Y + VY
-    
-    def move_vel():
-        VX = VX + AX
-        VY = VY + AY
-    
-    def accel(id):
-        ones = np.ones((body_count,body_count))
-        eyes = np.eye(body_count)
-    
-        AX2d = G*m*(X*ones - (X*ones).T) / (DIS**3 + eyes)
-        AX = np.sum(AX2d, axis = 1)
-        AY2d = G*m*(Y*ones - (Y*ones).T) / (DIS**3 + eyes)
-        AY = np.sum(AY2d, axis = 1)
-    
-        return AX, AY
-
-
-def main():
-
-	"""
-    Function:   main 		主函数
-    Input:      None
-    Output:     Unknown 	模拟画面
-    """
-	for i in range(1, max_count + 1):
-        body(i,
-             random.randint(50, 150),           # 初始质量
-             random.randint(0, width),
-             random.randint(0, height),         # 初始坐标
-             random.randint(-120, 120)/100,
-             random.randint(-120, 120)/100)     # 初始速度
-    body_count = len(body_list)
-    univ1 = universe(NUM, MASS, X, Y, VX, VY)
-
-    
-
-
 
 if __name__ == '__main__':
-	main()
+
+    #middlex, middley = width // 2, height // 2          # 中心点坐标
+    
+    running = True                          # 程序运行状态
+
+    for i in range(1, max_count + 1):
+        body(i,
+             random.randint(150, 300),           # 初始质量
+             random.randint(0, width),
+             random.randint(0, height),         # 初始坐标
+             random.randint(-120, 120)/500,
+             random.randint(-120, 120)/500)     # 初始速度
+    body_count = len(body_list)
+
+    while True:
+
+        tic = time.time()
+
+        if running:
+            screen = np.zeros((height, width, 3), dtype = np.uint8)
+
+            drawStars(X, Y, R, COLOR)
+
+            accel()
+            move_vel()
+            move_pos()
+
+        cv.imshow("Universe", screen)
+
+        toc = time.time()
+        print(toc - tic)
+
+        key = cv.waitKey(50)
+
+        if key == 27:                                      # 按esc退出
+            break
+            
+        if key == 9:                                       # Tab，Pause或Resume
+            running = not running;
+
+    for i in range(5):                                     # 关闭cv窗口
+        cv.waitKey(1)
+        cv.destroyAllWindows()
+
+    
+
+
+
+
